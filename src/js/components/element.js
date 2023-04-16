e86948fe3f82a15dafba01a38104a82d0fbd5155
Владислав Белов <belov_vlad@list.ru>
2023-04-16 18:40:54 +0300

    16

--- src/js/components/element.js
+++ src/js/components/element.js
@@ -1,6 +1,10 @@
 
 const sity = document.querySelector('#sity');
 const region = document.querySelector('#category');
+const catalog = document.querySelector('#catalogcategory');
+const price = document.querySelector('#price');
+const discount = document.querySelector('#discount');
+const color = document.querySelector('#color');
 
 
 const sitySelect = new Choices( sity, {
@@ -89,6 +93,41 @@ regionSelect.setValue([
   { value: 'Кровати', label: 'Кровати' },
   { value: 'Аксессуары', label: 'Аксессуары' },
   { value: 'Тумбы', label: 'Тумбы' },
+]);
+
+const catalogSelect = new Choices(catalog,{
+ placeholder: true,
+
+});
+
+
+catalogSelect.setValue([
+
+{ value: 'Диваны', label: 'Диваны' },
+{ value: 'Кресла', label: 'Кресла' },
+{ value: 'Пуфы', label: 'Пуфы' },
+{ value: 'Столы', label: 'Столы' },
+{ value: 'Стулья', label: 'Стулья' },
+{ value: 'Комоды', label: 'Комоды' },
+{ value: 'Кровати', label: 'Кровати' },
+{ value: 'Аксессуары', label: 'Аксессуары' },
+{ value: 'Тумбы', label: 'Тумбы' },
+ 
+]);
+
+const priceSelect = new Choices(price);
+priceSelect.setValue([
+
+]);
+
+const discountSelect = new Choices(discount);
+discountSelect.setValue([
+
+]);
+
+const colorSelect = new Choices(color);
+colorSelect.setValue([
+
 ]);
 
 
