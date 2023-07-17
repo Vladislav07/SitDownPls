fbe4eff18cb620336b83974969e70c21a595b842
Владислав Белов <belov_vlad@list.ru>
2023-07-17 21:33:34 +0300

    17-1

--- src/js/components/action.js
+++ src/js/components/action.js
@@ -6,3 +6,31 @@
 // });
 
 
+ const buttonsCatalog = document.querySelectorAll('.catalogue__btn');
+ const cards = document.querySelectorAll('.card');
+
+ buttonsCatalog.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      e.preventDefault()
+      const actualDisplay = getComputedStyle(buttonsCatalog[2]).display;
+      if (actualDisplay == 'none') {
+        if(e.target.id === 'Page_1'){
+          hiddenCards(1, 9)
+        }else{
+          hiddenCards(10, 18)
+        }
+      }
+     
+    });
+  });
+
+  function hiddenCards(start, end){
+     for (let index = 1; index <= 18; index++) {
+      const element = cards[index];
+       if(index => start && index <= end){
+        elem.classList.toggle("class")
+       }else{
+        element.style.display = 'none'
+       }
+     }
+  }
